7c8464d9cebf5a8918d056fd37c1ad3af63aa7fe
caceresj21 <caceresalbertogal10@gmail.com>
2023-04-22 08:40:10 -0600

    Add project files.

---
+++ L02_P02_ 2018-RR-604_ 2015-CG-601/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using L02_P02__2018_RR_604__2015_CG_601.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace L02_P02__2018_RR_604__2015_CG_601.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L02_P02_ 2018-RR-604_ 2015-CG-601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02_P02__2018_RR_604__2015_CG_601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
